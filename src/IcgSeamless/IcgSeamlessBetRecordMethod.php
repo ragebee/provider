4e6ba00b662e79be4051601e3a93ae3c19de2b1b
Fish <chao.yen.po@gmail.com>
2020-10-30 11:53:50 +0800

    Base icg

---
+++ src/IcgSeamless/IcgSeamlessBetRecordMethod.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Ragebee\Provider\IcgSeamless;
+
+use Carbon\Carbon;
+use Ragebee\FishpondRecord\AbstractBetRecordMethod;
+use Ragebee\FishpondRecord\BetRecordInterface;
+use Ragebee\FishpondRecord\DisplayData;
+use Ragebee\Fishpond\Config;
+
+class IcgSeamlessBetRecordMethod extends AbstractBetRecordMethod
+{
+    protected $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    public function getCreatedAt($betRecord): ?Carbon
+    {
+        return Carbon::parse(data_get($betRecord, 'createdAt'));
+    }
+
+    public function getUpdatedAt($betRecord): ?Carbon
+    {
+        return Carbon::parse(data_get($betRecord, 'updatedAt'));
+    }
+
+    public function getBetId($betRecord): string
+    {
+        return data_get($betRecord, 'id');
+    }
+
+    public function getRoundId($betRecord): string
+    {
+        return data_get($betRecord, 'setId');
+    }
+
+    public function getPlayerName($record): string
+    {
+        return data_get($record, 'player');
+    }
+
+    public function getGameCode($record): string
+    {
+        return data_get($record, 'gameId');
+    }
+
+    public function getStatus($record): int
+    {
+        $status = data_get($betRecord, 'status');
+
+        switch ($status) {
+            case 'playing':
+                return BetRecordInterface::STATUS_ACTIVE;
+            case 'cancel':
+                return BetRecordInterface::STATUS_CANCEL;
+            case 'finish':
+                return BetRecordInterface::STATUS_COMPLETED;
+            default:
+                return BetRecordInterface::STATUS_TO_BE_DETERMINED;
+        }
+    }
+
+    public function getBetAmount($betRecord): string
+    {
+        return (string) (data_get($betRecord, 'bet') / 100);
+    }
+
+    public function getValidBetAmount($betRecord): string
+    {
+        return (string) (data_get($betRecord, 'validBet') / 100);
+    }
+
+    public function getPayment($betRecord): ?string
+    {
+        return (string) (data_get($betRecord, 'validBet') / 100);
+    }
+
+    public function getWinloss($betRecord): ?string
+    {
+        return null;
+    }
+
+    public function getDisplayData($betRecord): array
+    {
+        $displayData = new DisplayData($betRecord);
+
+        return $displayData->toArray();
+    }
+}
---
+++ src/IcgSeamless/IcgSeamlessClient.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Ragebee\Provider\IcgSeamless;
+
+use GrahamCampbell\GuzzleFactory\GuzzleFactory;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+
+class IcgSeamlessClient
+{
+    const API_URL = 'https://uat-wb-api.jlfafafa2.com/api1/';
+
+    /** @var string */
+    protected $apiUrl;
+
+    /** @var \GuzzleHttp\ClientInterface */
+    protected $client;
+
+    /** @var array */
+    protected $credentials;
+
+    /**
+     * - api_url:
+     *   (string)
+     * - credentials:
+     *   (array) an array of "agent_id", "agent_key" key.
+     *
+     * @param array $args
+     */
+    public function __construct(array $args)
+    {
+        $this->apiUrl = $args['api_url'] ?? self::API_URL;
+        $this->client = $args['client'] ?? new GuzzleClient(['handler' => GuzzleFactory::handler()]);
+        $this->credentials = $args['credentials'];
+    }
+
+    public function getGameList($lang = 'zh')
+    {
+        $parameters = [
+            'type' => 'slot',
+            'lang' => $lang,
+        ];
+
+        $response = $this->client->GET($this->getEndpointUrl($this->apiUrl, '/api/v1/games'), [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer ' . $this->credentials['token'],
+            ],
+            RequestOptions::QUERY => $parameters,
+        ]);
+
+        return json_decode($response->getBody(), true) ?: (string) $response->getBody();
+    }
+
+    public function createPlayer(Player $player): Result
+    {
+        $response = $this->httpClient->post($this->apiUrl . '/api/v1/players', [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer ' . $this->credentials['token'],
+            ],
+            RequestOptions::JSON => [
+                'username' => $player->name,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true) ?: (string) $response->getBody();
+    }
+
+    public function loginGame(
+        string $token,
+        string $homeUrl,
+        string $lang = 'zh',
+        string $href
+    ) {
+        $parameters = [
+            'token' => $token,
+            'home_URL' => $homeUrl,
+            'Lang' => $lang,
+        ];
+
+        return [
+            'Url' => $$href . http_build_query($parameters),
+        ];
+    }
+
+    /**
+     * 取得玩家下注紀錄
+     *
+     * Time format: 時區 +0, 需補到毫秒 Ex. 1566230400000
+     *
+     * @return array|null
+     */
+    public function getBetRecord(string $startTime, string $endTime, int $page = 1, int $pagesize = 500)
+    {
+        if ($pagesize < 10 || $pagesize > 10000) {
+            throw new InvalidArgumentException('Pagesize need > 10 and <= 10000. pagesize:' . $pagesize);
+        }
+
+        $parameters = [
+            'start' => $startTime,
+            'end' => $endTime,
+            'page' => $page,
+            'pageSize' => $pagesize,
+        ];
+
+        $parameters['Key'] = $this->getKey($parameters);
+
+        $response = $this->client->get($this->getEndpointUrl($this->apiUrl, '/api/v1/profile/rounds'), [
+            RequestOptions::QUERY => $parameters,
+        ]);
+
+        return json_decode($response->getBody(), true) ?: (string) $response->getBody();
+    }
+
+    protected function getEndpointUrl(string $url, string $endpoint): string
+    {
+        return "{$url}/{$endpoint}";
+    }
+}
